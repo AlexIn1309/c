392f098926f1ae28bddf09504c61a1f58604d802
alex <cesar@icloud.com>
2026-07-25 20:39:31 -0600

    Bucle Do while

---
+++ bucleDoWhile/bucleDoWhile.c
@@ -0,0 +1,16 @@
+/* BUCLE DO WHILE
+ * Sintaxis
+ * do{
+ *	bloque de codigo;
+ * }while */ 
+
+#include <stdio.h>
+
+int main(){
+	int i = 0;
+	do{
+		printf("%i. \n", i);
+		i++;
+	}while(i!=1);
+	return 0;
+}
