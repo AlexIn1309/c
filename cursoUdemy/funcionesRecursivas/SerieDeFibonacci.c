dff6d5658a9b8e4c551f6eff161d9b4e089385cc
alejandro <pickledgob@icloud.com>
2026-08-06 22:20:49 -0600

    build: configurar .gitignore para filtrar binarios y mantener solo codigo fuente

--- cursoUdemy/funcionesRecursivas/SerieDeFibonacci.c
+++ cursoUdemy/funcionesRecursivas/SerieDeFibonacci.c
@@ -2,24 +2,23 @@
 
 #include <stdio.h>
 
-int serieDeFibonacci(int num);
+unsigned long long memo[100] = {0};
 
-int main(){
-	int numero = 0;
-	puts("Ingresa un numero: ");
-	scanf("%d", &numero);
-	puts("debug");
-	for (int i = 1; i<= numero; ++i) {
-		printf("%d\n",serieDeFibonacci(i));
-	}
-	return 0;
+unsigned long long serieDeFibonacci(int num) {
+    if (num == 1 || num == 2) return 1;
+    if (memo[num] != 0) return memo[num]; // Si ya se calculó, se devuelve de inmediato
+
+    memo[num] = serieDeFibonacci(num - 1) + serieDeFibonacci(num - 2);
+    return memo[num];
 }
 
-int serieDeFibonacci(int num){
-
-	if(num == 1 || num == 2){
-		return 1;
-	} else {
-		return (serieDeFibonacci(num-1) + serieDeFibonacci(num-2));
-	}
+int main() {
+    int numero = 0;
+    puts("Ingresa un numero: ");
+    scanf("%d", &numero);
+    
+    for (int i = 1; i <= numero; ++i) {
+        printf("%llu\n", serieDeFibonacci(i));
+    }
+    return 0;
 }
