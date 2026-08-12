0f742f9430c3af696b72570373d71ee7e50f7a97
alejandro <pickledgob@icloud.com>
2026-08-12 17:26:06 -0600

    Copiar un arreglo a otro arreglo

--- cursoUdemy/Arreglos2/CopiarArregloAOtro.c
+++ cursoUdemy/Arreglos2/CopiarArregloAOtro.c
@@ -3,12 +3,28 @@
 #include <stdio.h>
 
 void copiandoArreglos(int arrA[], int arrb[], int tamano);
+void imprimirArreglo(int arregloImprimir[]);
 
 int main(){
-	d
+	int arreglo1[5] = {2,3,4,5,6};
+	int arregloDos[5];
+	imprimirArreglo(arregloDos);
+
+	printf("Copiando arreglo1 a arregloDos...\n");
+	copiandoArreglos(arreglo1,arregloDos,5);
+	imprimirArreglo(arregloDos);
+
 	return 0;
 }
 
-void copiandoArreglos(int arrA[], int arrb[], int tamano){
-	d
+void copiandoArreglos(int arrA[], int arrB[], int tamano){
+	for (int i = 0; i < tamano; i++) {
+		arrB[i] = arrA[i];
+	}
+}
+
+void imprimirArreglo(int arregloImprimir[]){
+	for (int i = 0; i < 5; i++) {
+		printf("%i", arregloImprimir[i]);
+	}
 }
