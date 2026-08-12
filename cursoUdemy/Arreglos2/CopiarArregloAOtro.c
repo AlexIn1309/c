e8db50ceba9713a4aefe0ef8e6b44727dd773397
alejandro <pickledgob@icloud.com>
2026-08-12 14:57:24 -0600

    Creando actividad de arreglos

---
+++ cursoUdemy/Arreglos2/CopiarArregloAOtro.c
@@ -0,0 +1,14 @@
+/* COPIAR UN ARREGLO A OTRO */
+
+#include <stdio.h>
+
+void copiandoArreglos(int arrA[], int arrb[], int tamano);
+
+int main(){
+	d
+	return 0;
+}
+
+void copiandoArreglos(int arrA[], int arrb[], int tamano){
+	d
+}
